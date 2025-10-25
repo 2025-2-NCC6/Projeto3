ac5d8d5deee0e66d284ca66995ccddfd35a39b72
VinnizzZ <vinnibinda@hotmail.com>
2025-10-24 21:44:43 -0300

    Teste de config inicial webserver

--- servidor/server.py
+++ servidor/server.py
@@ -3,12 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
-import database
 import logging
 
 # IP e porta do servidor
-IP_LOCAL = os.getenv("ip")
-PORTA_LOCAL = int(os.getenv("porta_server"))
+IP_LOCAL = '0.0.0.0'
+PORTA_LOCAL = 8000
 
 # Configuração de logging
 logging.basicConfig(
@@ -21,22 +20,12 @@ logging.basicConfig(
 ipCliente = None
 portaCliente = None
 
-# Conexão com o DB
-load_dotenv()
-
-usuario = os.getenv("usuario")
-senha = os.getenv("senha")
-host = os.getenv("host")
-porta = os.getenv("porta")
-banco = os.getenv("banco")
-
-engine = create_engine(
-    f'mysql+pymysql://{usuario}:{senha}@{host}:{porta}/{banco}',
-    pool_pre_ping=True, pool_recycle=3600
-)
-
 # Função para lidar com os requests
 def handle_request(client_socket):
+
+    global status1
+    global status2
+
     request_data = client_socket.recv(1024).decode('utf-8')
     print(f"Received request:\n{request_data}")
 
@@ -53,17 +42,141 @@ def handle_request(client_socket):
 
             # Rotas do servidor
             if method == 'GET' and path == '/':
-                response_body = "<h1>Funcionando</h1>"
+                status1 = False
+                status2 = False
+
+                if status1:
+                    text1 = 'ON'
+                else:
+                    text1 = 'OFF'
+
+                if status2:
+                    text2 = 'ON'
+                else:
+                    text2 = 'OFF'
+
+                response_body = f'''
+                  <!DOCTYPE html><html>
+                    <head>
+                      <title>ESP32 Web Server Demo</title>
+                      <meta name="viewport" content="width=device-width, initial-scale=1">
+                      <style>
+                        html {{ font-family: sans-serif; text-align: center; }}
+                        body {{ display: inline-flex; flex-direction: column; }}
+                        h1 {{ margin-bottom: 1.2em; }}
+                        h2 {{ margin: 0; }}
+                        div {{ display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: auto auto; grid-auto-flow: column; grid-gap: 1em; }}
+                        .btn {{ background-color: #5B5; border: none; color: #fff; padding: 0.5em 1em;
+                               font-size: 2em; text-decoration: none }}
+                        .btn.OFF {{ background-color: #333; }}
+                      </style>
+                    </head
+                    <body>
+                      <h1>ESP32 Web Server</h1
+                      <div>
+                        <h2>LED 1</h2>
+                        <a href="/toggle/1" class="btn {text1}">{text1}</a>
+                        <h2>LED 2</h2>
+                        <a href="/toggle/2" class="btn {text2}">{text2}</a>
+                      </div>
+                    </body>
+                  </html>
+                '''
                 response_headers = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"
                 response = (response_headers + f"Content-Length: {len(response_body.encode('utf-8'))}\r\n\r\n" + response_body).encode('utf-8')
                 for i in range(len(request_lines)):
                     logging.info(f"Received request:\n{request_lines[i]}")
-            elif method == 'GET' and path == '/teste':
-                response_body = "<h1>TESTE</h1>"
+
+            elif method == 'GET' and path == '/toggle/1':
+                status1 = not status1
+
+                if status1:
+                    text1 = 'ON'
+                else:
+                    text1 = 'OFF'
+
+                if status2:
+                    text2 = 'ON'
+                else:
+                    text2 = 'OFF'
+
+                response_body = f'''
+                  <!DOCTYPE html><html>
+                    <head>
+                      <title>ESP32 Web Server Demo</title>
+                      <meta name="viewport" content="width=device-width, initial-scale=1">
+                      <style>
+                        html {{ font-family: sans-serif; text-align: center; }}
+                        body {{ display: inline-flex; flex-direction: column; }}
+                        h1 {{ margin-bottom: 1.2em; }}
+                        h2 {{ margin: 0; }}
+                        div {{ display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: auto auto; grid-auto-flow: column; grid-gap: 1em; }}
+                        .btn {{ background-color: #5B5; border: none; color: #fff; padding: 0.5em 1em;
+                               font-size: 2em; text-decoration: none }}
+                        .btn.OFF {{ background-color: #333; }}
+                      </style>
+                    </head
+                    <body>
+                      <h1>ESP32 Web Server</h1
+                      <div>
+                        <h2>LED 1</h2>
+                        <a href="/toggle/1" class="btn {text1}">{text1}</a>
+                        <h2>LED 2</h2>
+                        <a href="/toggle/2" class="btn {text2}">{text2}</a>
+                      </div>
+                    </body>
+                  </html>
+                '''
                 response_headers = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"
                 response = (response_headers + f"Content-Length: {len(response_body.encode('utf-8'))}\r\n\r\n" + response_body).encode('utf-8')
                 for i in range(len(request_lines)):
                     logging.info(f"Received request:\n{request_lines[i]}")
+
+            elif method == 'GET' and path == '/toggle/2':
+                status2 = not status2
+
+                if status1:
+                    text1 = 'ON'
+                else:
+                    text1 = 'OFF'
+
+                if status2:
+                    text2 = 'ON'
+                else:
+                    text2 = 'OFF'
+
+                response_body = f'''
+                  <!DOCTYPE html><html>
+                    <head>
+                      <title>ESP32 Web Server Demo</title>
+                      <meta name="viewport" content="width=device-width, initial-scale=1">
+                      <style>
+                        html {{ font-family: sans-serif; text-align: center; }}
+                        body {{ display: inline-flex; flex-direction: column; }}
+                        h1 {{ margin-bottom: 1.2em; }}
+                        h2 {{ margin: 0; }}
+                        div {{ display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: auto auto; grid-auto-flow: column; grid-gap: 1em; }}
+                        .btn {{ background-color: #5B5; border: none; color: #fff; padding: 0.5em 1em;
+                               font-size: 2em; text-decoration: none }}
+                        .btn.OFF {{ background-color: #333; }}
+                      </style>
+                    </head
+                    <body>
+                      <h1>ESP32 Web Server</h1
+                      <div>
+                        <h2>LED 1</h2>
+                        <a href="/toggle/1" class="btn {text1}">{text1}</a>
+                        <h2>LED 2</h2>
+                        <a href="/toggle/2" class="btn {text2}">{text2}</a>
+                      </div>
+                    </body>
+                  </html>
+                '''
+                response_headers = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"
+                response = (response_headers + f"Content-Length: {len(response_body.encode('utf-8'))}\r\n\r\n" + response_body).encode('utf-8')
+                for i in range(len(request_lines)):
+                    logging.info(f"Received request:\n{request_lines[i]}")
+                    
             else:
                 response_body = "<h1>404 Not Found</h1>"
                 response_headers = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n"
