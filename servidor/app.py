ca17d85707ff9dcf9528fdbc648b7722964c39a1
Vinicius Binda <vinnibinda@hotmail.com>
2025-11-03 19:00:29 -0300

    Conexao do server com o esp

--- servidor/app.py
+++ servidor/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
-ESP32_IP = "http://172.20.10.5"
+ESP32_IP = "http://192.168.15.15"
 
 solda = False
 ferramentas = {
@@ -40,24 +40,27 @@ def toggle_solda():
     global solda
     solda = not solda
 
-    status = request.json.get('action')
+    energia = request.json.get('action')
     
     status_texto = "LIGADO" if solda else "DESLIGADO"
-    print(f"Comando recebido: BANCADA DE SOLDA: {status_texto}")
+    status_comando = "ON" if solda else "OFF"
+
+    print(f"Comando recebido: BANCADA DE SOLDA: {status_texto}. Enviando '{status_comando}' para o ESP32.")
     
     try:
-        response = requests.get(f"{ESP32_IP}/solda?estado={status}", timeout=5)
-        response.raise_for_status()
-    
+        response = requests.get(f"{ESP32_IP}/solda_toggle?state={status_comando}", timeout=5)
+        response.raise_for_status() 
+
         return jsonify({
             'status': 'sucesso', 
             'novo_estado': solda,
             'novo_texto': status_texto
         })
-    
+
     except requests.exceptions.RequestException as e:
-        print(f"ERRO ao comunicar com o ESP32: {e}")
-        return jsonify({'status': 'erro', 'mensagem': 'Falha na comunicação com o hardware'}), 500
+        print(f"ERRO ao comunicar com o ESP32 para a Bancada de Solda: {e}")
+        solda = not solda
+        return jsonify({'status': 'erro', 'mensagem': 'Falha na comunicação com o ESP32 para a solda'}), 500
 
 # Rota ferramentas
 @app.route('/status_ferramentas', methods=['GET'])
